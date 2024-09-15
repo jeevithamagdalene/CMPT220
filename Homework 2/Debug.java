3f7304f0ca043b68e662acd9fab32b410b0d8b70
jeevithamagdalene <jeevithamagdalene.jeevan1@marist.edu>
2024-09-15 13:22:03 -0400

    Homewor Debug Finished

--- Homework 2/Debug.java
+++ Homework 2/Debug.java
@@ -1,39 +1,46 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
 
-public class Main {
+
+public class Debug {
     public static void main(String[] args) {
 
         //for this section: are these all printing the best option? If they aren't, fix it!
         //(However you interpret 'fix' is fine i promise, any way you fix it shows you get the concept to me)
         int var1 = 4;
         if (var1 > 4){
-            System.out.println("Var1 is greater than 4");
-        }else{
-            System.out.println("Var1 is less than 4");
+            System.out.println(var1+ " is greater than 4");
+        }
+        else if(var1 ==4){
+            System.out.println(var1 + " is 4");
+        }
+         else{
+            System.out.println(var1 + " is less than 4");
         }
 
         int var2 = 6;
         if (var2 == 5){
-            System.out.println("Var2 is 5");
+            System.out.println(var2 + " is 5");
         } else if (var2 > 5){
-            System.out.println("Var2 is greater than 5");
+            System.out.println(var2 + " is greater than 5");
         } else if (var2 < 5){
-            System.out.println("Var2 is less than 5");
+            System.out.println(var2 + " is less than 5");
         } else{
-            System.out.println("Var2 is 6");
+            System.out.println( var2 + " is 6");
         }
 
         int var3 = 5;
         if (var3 > 10){
-            System.out.println("Var3 is greater than 10");
+            System.out.println(var3 + " is greater than 10");
         } else {
-            System.out.println("Var3 is less than 10");
+            System.out.println(var3 + " is less than 10");
         }
 
-
-
-
         //for this section: why are we not entering the if statement?
+        //Because captilization matters in Java! since the first letter of "Marist" is different(one is captilized and another is small),
+        //therefore it didn't pass the if statement
         if ("Marist" == "marist"){
+        
             System.out.println("Marist college!");
         } else{
             System.out.println("Not marist college :(!");
--- Homework 2/ifelsepractice.java
+++ Homework 2/ifelsepractice.java
@@ -8,12 +8,64 @@ import java.util.Scanner;
 * can be any topic you pick :) feel free to pick some obscure or niche topics!
 * good luck!
 * */
-public class Main {
+public class ifelsepractice {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a=0;
+        System.out.println("Q1: What is the Capital of Inida? ");
+        String answer1 = sc.nextLine();
+        if(answer1.equals("Delhi"))
+        {
+            System.out.println("It is Right Answer !");
+            a++;
+        }
+        else if(answer1.equals("delhi"))
+        {
+            System.out.println("It is Right Answer !");
+            a++;
+        }
+        else if(answer1.equals("DELHI"))
+        {
+            System.out.println("It is Right Answer !");
+            a++;
+        }
+        else
+        {
+            System.out.println("It is wrong Answer !");
+        }
+            
+       System.out.println("Q2: What is 2+2? ");
+       int answer2 = sc.nextInt();
+       if(answer2 == 4)
+        {
+            System.out.println("It is Right Answer !");
+            a++;
+        }
+        else{
+            System.out.println("It is wrong Answer !");
+        }
 
+       System.out.println("Q2: Tell me any even Number? ");
+        int answer3 = sc.nextInt();
 
+        if (answer3 == 0)
+        {
+            System.out.println("0 is neither and even or Odd Number, So Its a Wrong answer !");
+        }
+        else if(answer3 % 2 == 0 ){
+            System.out.println("It is Right Answer !");
+            a++;
+        }
+        else{
+            System.out.println("It is wrong Answer !");
+        }
 
+        System.out.println("Your Score is : " + a);
     }
+   
+    
+
+   
 }
 
 
