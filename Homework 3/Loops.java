38bf7af1779fcb9f82de892566dc7510bb39f78a
jeevithamagdalene <jeevithamagdalene.jeevan1@marist.edu>
2024-09-24 12:40:53 -0400

    Homework 3 Finished

--- Homework 3/Loops.java
+++ Homework 3/Loops.java
@@ -8,11 +8,23 @@ how did you figure out how to do this? can you translate your code into simple t
 you need to explain why you picked your for loop conditional and what's doing the work with the string
 if you're confused reach out!
  */
-
-public class Main {
+import java.util.Scanner;
+public class Loops {
     public static void main(String[] args) {
-
-
+        //Step1: The scanner allows me to take the input from the user in the console
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter a Word:");
+     //Step 2: This take the string input from the user and store it in a variable
+        String answer1 = scanner.nextLine();  
+    //Step 3: For loop to iterate each letter from the word. Starting from the 0th index(1st letter) till the 
+    //last word. To find the last word,  need to take length of the word by using the length function
+    //Need to iterate from the 0th index of the word until length-1 times to get the full word.
+    
+        for (int i = 0; i <  answer1.length(); i++) { 
+            // Step 4: answer1.charAt(i) is used to get the character at index i in the string.
+            System.out.println(answer1.charAt(i));
+        }
+    
 
     }
 }
--- Homework 3/LoopsDebug.java
+++ Homework 3/LoopsDebug.java
@@ -1,11 +1,11 @@
-
-public class Main {
+import java.util.Scanner;
+public class LoopsDebug {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
 
         //P1: This one only prints 0-9, can you fix it so it prints 1-10?
         System.out.println("Problem 1");
-        for (int i = 0; i < 10; i++){
+        for (int i = 1; i <= 10; i++){
             System.out.println(i);
         }
 
@@ -13,29 +13,48 @@ public class Main {
         //(factorial = X!, X being the user input, Factorials are every digit before X multiplied together)
         System.out.println("Problem 2");
         System.out.println("Enter a number and I will tell you the fatorial: ");
+        int answer2 = sc.nextInt();
+        int product = 1;
         //here's a hint
-        for (int i = 0; i < ?; i++){
+        for (int i = 1; i <= answer2; i++){
+          product = product*i;
 
         }
+        System.out.println(product);
 
         //P3: Ask the user for a number, and then add together every OTHER digit (starting from 1)
         System.out.println("Problem 3");
         System.out.println("Enter a number and I will tell you the sum of every other number: ");
+        int sum =0;
         //No hint! what do you need to complete this task?
+        int answer3 = sc.nextInt();
+        for (int j= 1; j< answer3; j+=2){
 
+           sum = sum +j;
+          
+        }
+        System.out.println(sum);
 
         //P4: Why does this loop never stop!
         //what can you do to break out of the loop after it prints once?
+        //after printing once , Need to set the run as fasle, to not to satisfy the condition on while loop
         System.out.println("Problem 4");
         boolean run = true;
         while (run == true){
             System.out.println("I printed once!");
+            run = false;
+
         }
 
         //P5: Take a string from the user and print them the reverse!
         System.out.println("Problem 5");
         //hint
-        String reverse = "";
-
+        sc.nextLine(); 
+        System.out.print("Enter a string to reverse: ");
+        String reverse = sc.nextLine();
+        for (int i = reverse.length() - 1; i >= 0; i--) {
+            System.out.print(reverse.charAt(i));
     }
+
+}
 }
