1e86240c74b2e9d1488f1702836c207d72f617a2
jeevithamagdalene <jeevithamagdalene.jeevan1@marist.edu>
2024-12-10 12:52:35 -0500

    Final Assignment Finished.

--- Milloinaire.java
+++ Milloinaire.java
@@ -30,7 +30,7 @@ public class Millionaire {
             {"What is the square root of 64?", new String[]{"6", "8", "10", "12"}, "8"}
         };
 
-        // Loop through the question
+        // Loop through the questions
         for (Object[] questionData : questions) {
             String questionText = (String) questionData[0];
             String[] options = (String[]) questionData[1];
